da479cabc79a1137f84264e441f93c401666614c
Rodrigo <rodrigosangali@gmail.com>
2022-01-28 07:41:04 -0300

    test

---
+++ src/main/java/com/sangali/poc_spring_boot_write_mysql/PocSpringBootWriteMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.sangali.poc_spring_boot_write_mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocSpringBootWriteMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocSpringBootWriteMysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sangali/poc_spring_boot_write_mysql/PocSpringBootWriteMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sangali.poc_spring_boot_write_mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocSpringBootWriteMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
